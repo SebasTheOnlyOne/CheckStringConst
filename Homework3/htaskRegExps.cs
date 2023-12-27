d9837b4ea6337c78bbdff1958e0a9ddf1edff588
SebasTheOnlyOne <ilya.skvortsov.2004@mail.ru>
2023-12-27 16:56:08 +0300

    Добавьте файлы проекта.

---
+++ Homework3/htaskRegExps.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Homework3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку, в которой необходимо найти российские автомобильные номера: ");
+            var stringAuto = Console.ReadLine();
+            Console.WriteLine($"Результат: {SearchAutoNum(stringAuto)}");
+            Console.WriteLine("Введите строку, в которой необходимо найти IPv4 адреса: ");
+            var stringIP = Console.ReadLine();
+            Console.WriteLine($"Результат: {SearchIPv4(stringIP)}");
+            Console.WriteLine("Введите текст, где одинарные звёздочки надо заменить на теги: ");
+            var stringTeg = Console.ReadLine();
+            Console.WriteLine($"Результат: {SwapToTeg(stringTeg)}");
+            Console.WriteLine("Введите строку, которую надо проверить на принадлежность к доменным протолокам https или http: ");
+            var stringProtoc = Console.ReadLine();
+            Console.WriteLine($"Результат: {stringProtoc} - {ItIsHttp(stringProtoc)}");
+
+        }
+
+        //Выяснить, какими могут быть российские автомобильные номера (с кодом региона), составить соответствующее регулярное выражение и
+        //написать программу, которая находит в строке все автомобильные номера и выводит их на консоль.
+        public static string SearchAutoNum (string s)
+        {
+            var count = 1;
+            var s1 = "";
+            foreach (Match m in Regex.Matches(s, @"\b[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2}1?\d{2}\b"))
+            {
+                s1 += $"{count}: {m.Value} ";
+                count++;
+            }
+            return s1;
+        }
+
+        //Дана строка. Вывести все содержащиеся в ней IPv4-адреса в десятичной записи с точками через разделитель.
+        public static string SearchIPv4 (string s)
+        {
+            var count = 1;
+            var s1 = "";
+            foreach (Match m in Regex.Matches(s, @"(\b\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\b"))
+            {
+                if (int.Parse(m.Groups[1].ToString()) <= 255)
+                {
+                    if(int.Parse(m.Groups[2].ToString()) <= 255)
+                    {
+                        if (int.Parse(m.Groups[3].ToString()) <= 255)
+                        {
+                            if (int.Parse(m.Groups[1].ToString()) <= 255)
+                            {
+                                s1 += $"{count}: {m.Value} ";
+                                count++;
+                            }
+                        }    
+                    }
+                } 
+            }
+            return s1;
+        }
+
+        //Преобразовать текст, обрамленный в звездочки, в текст обрамленный тегом <em></em>, т.е. курсив.
+        ////Не трогать текст в двойных звездочках (жирный). 
+        public static string SwapToTeg (string s)
+        {
+            s = Regex.Replace(s, @"\b\*(?!\*)", @"</em>");
+            s = Regex.Replace(s, @"(?<!\*)\*\b", @"<em>");
+            return s;
+        }
+
+        //Определить является ли строка доменным именем с протоколами http и https,
+        //с необязательным слешем в конце. Специальные символы не используются.
+        public static string ItIsHttp (string s)
+        {
+            var s1 = "Нет";
+            var m = Regex.Match(s, @"http[s]?://w+\.\w+");
+            if (m.Success == true)
+                s1 = "Да";
+            return s1;
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,101 @@
+using NUnit.Framework;
+using Homework3;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var testString = "dsfÀ213ÌÂ20   ûâä À213ÌÂ";
+            Assert.AreEqual(Program.SearchAutoNum(testString), "");
+            
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var testString1 = "dsfÀ213ÌÂ20   ûâä À213ÂÂ154";
+            Assert.AreEqual(Program.SearchAutoNum(testString1), "1: À213ÂÂ154 ");
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var testString2 = "dsfÀ213ÂÂ20   ûâä À213ÂÂ154 À213ÂÂ23";
+            Assert.AreEqual(Program.SearchAutoNum(testString2), "1: À213ÂÂ154 2: À213ÂÂ23 ");
+        }
+        [Test]
+        public void Test4()
+        {
+            var testString3 = "142.23.0.255 ";
+            Assert.AreEqual(Program.SearchIPv4(testString3), "1: 142.23.0.255 ");
+        }
+        [Test]
+        public void Test5()
+        {
+            var testString4 = "123.2344.12.23 dfsf233.32.32.32 dsdf 123.12.23.13";
+            Assert.AreEqual(Program.SearchIPv4(testString4), "1: 123.12.23.13 ");
+        }
+        [Test]
+        public void Test6()
+        {
+            var testString4 = "123.2344.12.23 dfsf233.32.32.32 dsdf 123.12.23.13 124.23.0.255";
+            Assert.AreEqual(Program.SearchIPv4(testString4), "1: 123.12.23.13 2: 124.23.0.255 ");
+        }
+        [Test]
+        public void Test7()
+        {
+            var testString5 = "*this is italic*";
+            Assert.AreEqual(Program.SwapToTeg(testString5), "<em>this is italic</em>");
+        }
+        [Test]
+        public void Test8()
+        {
+            var testString6 = "**bold text (not italic)**";
+            Assert.AreEqual(Program.SwapToTeg(testString6), "**bold text (not italic)**");
+        }
+        [Test]
+        public void Test9()
+        {
+            var testString7 = "** **";
+            Assert.AreEqual(Program.SwapToTeg(testString7), @"** **");
+        }
+        [Test]
+        public void Test10()
+        {
+            var testString7 = "*f*";
+            Assert.AreEqual(Program.SwapToTeg(testString7), @"<em>f</em>");
+        }
+        [Test]
+        public void Test11()
+        {
+            var testString8 = @"http://example.com/";
+            Assert.AreEqual(Program.ItIsHttp(testString8), @"Äà");
+        }
+        [Test]
+        public void Test12()
+        {
+            var testString9 = "example.com";
+            Assert.AreEqual(Program.ItIsHttp(testString9), @"Íåò");
+        }
+        [Test]
+        public void Test13()
+        {
+            var testString10 = "êðåìëü.ðô";
+            Assert.AreEqual(Program.ItIsHttp(testString10), @"Íåò");
+        }
+        [Test]
+        public void Test14()
+        {
+            var testString11 = @"https://example.com/";
+            Assert.AreEqual(Program.ItIsHttp(testString11), @"Äà");
+        }
+    }
+}
